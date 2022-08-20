9f771bcdf0f7dca47009e3f8ffe5392ac3a1f29c
Emelyanov9999 <emelyanov.v.s9999@gmail.com>
2022-08-20 22:39:01 +0300

    Lesson 2
    Практика по 2-му уроку.

--- Lesson 1/main.js
+++ Lesson 1/main.js
@@ -1,3 +1,23 @@
-console.log("Привет.Это мой первый урок по Java script.");
-console.log(50*2);
-console.log("Что произойдёт, если строку сложить с числом?" + 100);
+
+console.log("01");
+
+console.log('Добро'+' пожаловать'+'на курс,'+' Владимир');
+
+//alert("08.августа.2021");
+
+document.getElementById("one").innerHTML = "Hello World";
+
+//Числовой тип
+console.log(5)
+//Большое число
+console.log(1234567890n)
+//Строка
+console.log("Bleach Greatness")
+//Логический
+console.log(5>3)
+//Пустота 
+console.log(null)
+//Неопределён
+console.log(undefined)
+//type of
+console.log(typeof(1234567890n));
